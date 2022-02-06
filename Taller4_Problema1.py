95c2c594f137dc927eaf04801ec620dedaa5184c
Bichi <gnzlz.bianca@gmail.com>
2022-02-06 05:25:09 -0500

    Add files via upload

---
+++ Taller4_Problema1.py
@@ -0,0 +1,11 @@
+"""
+Taller 4 Problema 1
+Bianca Gonzalez
+Escribir los números del 0 al 9, utilizando una repetición.
+
+"""
+
+print("\nPrograma que imprime los numeros del 0 al 9\n")
+for n in range(10):
+    print(n,"\n")
+    
