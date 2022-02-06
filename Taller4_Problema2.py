b05c8e530c3b452e1d5e93e26131fe6fd1288fea
Bichi <gnzlz.bianca@gmail.com>
2022-02-06 05:29:16 -0500

    Add files via upload

---
+++ Taller4_Problema2.py
@@ -0,0 +1,10 @@
+"""
+Taller 4 Problema 2
+Bianca Gonzalez
+Escribir los números entre el 5 y el 20, saltando de tres en tres.
+"""
+
+print("\nPrograma que escribe los números entre 5 y 20 de 3 en 3\n")
+for n in range(5, 21, 3):
+    print(n,"\n")
+    
